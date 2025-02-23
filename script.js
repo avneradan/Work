2a38660468d4d11f0fc7e313a9091cac873e47bf
avneradan <avner.adan@gmail.com>
2025-02-23 14:38:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
-let mouseX = 0;
-let mouseY = 0;
-let cursorX = 0;
-let cursorY = 0;
+let mouseX = 0, mouseY = 0;
+let cursorX = 0, cursorY = 0;
 let firstMove = true;
 
 const customCursor = document.querySelector('.custom-cursor');
 
-// Listen for mousemove events
-document.addEventListener('mousemove', e => {
+document.addEventListener('mousemove', (e) => {
   mouseX = e.clientX;
   mouseY = e.clientY;
-
-  // On the very first mouse move, initialize cursorX/Y
   if (firstMove) {
     firstMove = false;
     cursorX = mouseX;
@@ -19,15 +14,11 @@ document.addEventListener('mousemove', e => {
   }
 });
 
-//  function animateCursor() {
-  // Lerp / smoothing
-//  const speed = 0.1;
-//  cursorX += (mouseX - cursorX) * speed;
-//  cursorY += (mouseY - cursorY) * speed;
-
-  // Position the cursor (center it by subtracting half of its size if desired)
+function animateCursor() {
+  const speed = 0.1; 
+  cursorX += (mouseX - cursorX) * speed;
+  cursorY += (mouseY - cursorY) * speed;
   customCursor.style.transform = `translate(${cursorX - 10}px, ${cursorY - 10}px)`;
-
   requestAnimationFrame(animateCursor);
 }
 
