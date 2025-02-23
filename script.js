0b2dcf59790e1e7df5bcb151ce5f9b14055dfe7a
avneradan <avner.adan@gmail.com>
2025-02-23 13:02:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 document.addEventListener("mousemove", (e) => {
     const customCursor = document.querySelector(".custom-cursor");
+
+    // If the element doesn't exist or there's some other issue, bail out
+    if (!customCursor) return;
+
     customCursor.style.left = e.pageX + "px";
     customCursor.style.top = e.pageY + "px";
 });
